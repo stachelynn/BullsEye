b9faa3fcdb536c7574958181edc67d25600490ea
Ashlynn Miyashiro <Ashlynn@rrcs-66-27-37-202.west.biz.rr.com>
2018-01-23 15:57:04 -1000

    created two IBActions

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        currentValue = lroundf(slider.value)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //slider method
+    @IBAction func sliderMoved(_ slider: UISlider){
+        print ("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func showAlert() {
+        
+        let message = "The value of the slider is: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello World!", message: message,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
